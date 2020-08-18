8b7763e1793fa9473cf5334d8b299bb41c0ad2cf
PJ Hoberman <phoberman@pocketoutdoormedia.com>
2020-08-17 20:05:21 -0600

    Updated table name

--- src/components/StateList.vue
+++ src/components/StateList.vue
@@ -40,7 +40,7 @@ export default {
       const Airtable = require('airtable');
       const base = new Airtable({apiKey: process.env.VUE_APP_AIRTABLE_API_KEY}).base('appUkL89RMW3J7G5t');
       const county_data = [{}, ];
-      base('County Drop Off Information').select({
+      base('County Local Election Information').select({
         filterByFormula: `State="${this.states[this.selectedState]['State/Territory']}"`,
         sort: [
           {field: 'County', direction: 'asc'}
