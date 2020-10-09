20c6256402fc1ecc84b6ed340a0bc5b7dd887619
ezekohon <ezekohon@gmail.com>
2020-10-09 10:15:38 -0300

    added picture tag with fallback to jpeg image

--- src/components/BallotDrop.vue
+++ src/components/BallotDrop.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="hello">
     <div id="stateSelector" class="text-center">
-      <img alt="Be Safe and Vote" src="../assets/be_safe_and_vote.webp" class="img-fluid">
+      <picture>
+        <source srcset="../assets/be_safe_and_vote.webp" type="image/webp">
+        <source srcset="../assets/be_safe_and_vote.jpg" type="image/jpeg">
+        <img src="../assets/be_safe_and_vote.jpg" alt="Be Safe and Vote">
+      </picture>
+      <!-- <img alt="Be Safe and Vote" src="../assets/be_safe_and_vote.webp" class="img-fluid"> -->
       <h1>{{ msg }}</h1>
       <h2>Save Democracy!</h2>
     </div>
